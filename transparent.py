f5f7cb516e086d43ee55071aadf2be61c3702bc1
jihyangleee <160590749+jihyangleee@users.noreply.github.com>
2024-12-11 15:11:38 +0900

    Update transparent.py

--- transparent.py
+++ transparent.py
@@ -18,8 +18,4 @@ def make_image_transparent(input_path, output_path, transparency=128):
 # 투명도를 50%로 설정한 이미지 생성
 make_image_transparent("RoomEscape//background.png", "RoomEscape//background(1).png", transparency=190)
 
-# # Bangtal에서 투명 이미지 사용
-# message_background = Object("RoomEscape//message_bg_transparent.png")
-# message_background.locate(scene3, 200, 300)
-# message_background.setScale(0.8)
-# message_background.show()
+
